ced885d7281e838629c5d725553ddf800d7a4e67
Andrew Bloom <blooma116@gmail.com>
2017-09-22 14:19:50 -0700

    added the third function

--- app.js
+++ app.js
@@ -55,11 +55,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var threeSum = sum(sum(a, b)[0], c)[0];
+  var threeProd = multiply(multiply(a, b)[0], c)[0];
+  var sumMessage = a + ' and ' + b + ' and ' + c + ' sum to ' + threeSum + '.';
+  var prodMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + threeProd + '.';
+  return [threeSum, threeProd, sumMessage, prodMessage];
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
